9273826ed666f0ffa5ceead576940336c3d4358a
ox <agual002@ucr.edu>
2021-08-13 16:45:46 -0700

    multiple flows version

--- src/mpExperienceQUIC.py
+++ src/mpExperienceQUIC.py
@@ -91,7 +91,9 @@ class MpExperienceQUIC(MpExperience):
 		self.compileGoFiles()
 		cmd = self.getQUICServerCmd()
 		#self.mpTopo.commandTo(self.mpConfig.server, "netstat -sn > netstat_server_before")
-		self.mpTopo.commandTo(self.mpConfig.server, cmd)
+		#self.mpTopo.commandTo(self.mpConfig.server, cmd)
+		self.mpTopo.commandTo(self.mpConfig.server, "./server_main -www . -certpath ~/go/src/github.com/lucas-clemente/quic-go/example/ -bind 10.1.0.1:6121 &>quic_server.log &")
+		self.mpTopo.commandTo(self.mpConfig.server, "./server_main -www . -certpath ~/go/src/github.com/lucas-clemente/quic-go/example/ -bind 10.1.0.1:6122 &>quic_server2.log &")
 
 		#self.mpTopo.commandTo(self.mpConfig.client, "sleep 2")
 
@@ -101,7 +103,8 @@ class MpExperienceQUIC(MpExperience):
 		#self.mpTopo.commandTo(self.mpConfig.client, cmd)
 
 		cmd = self.getQUICClientCmd()
-		self.mpTopo.commandTo(self.mpConfig.client, cmd)
+		self.mpTopo.commandTo(self.mpConfig.client, "./main -m -c https://10.1.0.1:6121/random &>quic_client.log &")
+		self.mpTopo.commandTo(self.mpConfig.client,"./main -m -c https://10.1.0.1:6122/random &>quic_client2.log &") 
 		#self.mpTopo.commandTo(self.mpConfig.server, "netstat -sn > netstat_server_after")
 		#self.mpTopo.commandTo(self.mpConfig.client, "netstat -sn > netstat_client_after")
 
